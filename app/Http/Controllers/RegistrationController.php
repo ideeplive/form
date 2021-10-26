3af59834a9239fb422b6b02e7416f9df354fe911
Home <ideeplive@gmail.com>
2021-10-26 14:53:13 +0530

    get post method update

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller{
+
+    // function signup(Request $request){
+    //     // dd($request);
+    //     return view('registration');
+    // }
+
+    // function showform(){
+    //     print_r('Get Method...');
+    //     return view('registration');
+    // }
+    // function signup(){
+    //     print_r('Post Method...');
+    //     return view('registration');
+    // }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+}
---
+++ resources/views/registration.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+</head>
+<body>
+    <form action="" method="POST">
+        @csrf
+        Name: <input type="text" name="name" id="name"> <br><br>
+        Email: <input type="email" name="email" id="email"> <br><br>
+        Password: <input type="password" name="password" id="password"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
